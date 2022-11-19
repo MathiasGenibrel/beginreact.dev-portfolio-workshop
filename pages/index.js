8c4ae89322763479da77fdece1418a1bfd432e55
MathiasGenibrel <mathias.geni@gmail.com>
2022-11-19 11:03:12 +0100

    🚧 Use Script to init Client with good theme

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { Footer } from "../src/components/Footer";
 import { Header } from "../src/components/Header";
 import { HeroSection } from "../src/components/hero";
 import { ProjectSection } from "../src/components/project";
+import Script from "next/script";
 
 // dynamic import Memory Section
 const DynamicMemorySection = dynamic(() => import("../src/components/memory"), {
@@ -18,6 +19,30 @@ const Home = () => {
       <Head>
         <title>Portfolio</title>
       </Head>
+      <Script strategy={"beforeInteractive"}>
+        {`
+          function setInitDefaultThemeColor() {
+            const userColorScheme = window.matchMedia("(prefers-color-scheme: light)")
+              .matches
+              ? "light"
+              : "dark";
+          
+            const storedColorScheme = localStorage.getItem("theme");
+          
+            const getColorScheme = () => {
+              if (storedColorScheme) return storedColorScheme === "light";
+              return userColorScheme === "light";
+            };
+          
+            const root = document.documentElement;
+            const isLightColorScheme = getColorScheme();
+          
+            if (!isLightColorScheme) root.querySelector("#app").classList.add("dark")
+          }
+        
+        document.addEventListener("DOMContentLoaded", setInitDefaultThemeColor);
+        `}
+      </Script>
       <div className="flex flex-col gap-40">
         <Header />
         <HeroSection />
